54b7214b42adec95168972f175dfa0416b5cbc05
ArtyomTkach <room5525969@mail.ru>
2021-05-05 09:36:10 +0300

    Updates

---
+++ precache-manifest.771452875da79ee27f5fb76157ba8aa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b320ef3cdab5d7f66f591bbb75c83d86",
+    "url": "/ReactWeather/index.html"
+  },
+  {
+    "revision": "c7a143873d56d6e57d2f",
+    "url": "/ReactWeather/static/css/main.22a73801.chunk.css"
+  },
+  {
+    "revision": "6d1a4b64c8a39ba56bd7",
+    "url": "/ReactWeather/static/js/2.d441107f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ReactWeather/static/js/2.d441107f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7a143873d56d6e57d2f",
+    "url": "/ReactWeather/static/js/main.ec195e75.chunk.js"
+  },
+  {
+    "revision": "32433c80a84bd06c2288",
+    "url": "/ReactWeather/static/js/runtime-main.d1504da6.js"
+  }
+]);
