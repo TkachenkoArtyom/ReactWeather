e218a8f80fa90e6b9511145e825015eabd9c57eb
ArtyomTkach <room5525969@mail.ru>
2021-05-05 09:39:55 +0300

    commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,3 @@
-export const apiGetWeather = `http://api.openweathermap.org/data/2.5/weather`;
+export const apiGetWeather = `https://api.openweathermap.org/data/2.5/weather`;
 export const units = 'metric';
 export const appid = '5ce78512a70c120ca1eeb0dac2f26106';
